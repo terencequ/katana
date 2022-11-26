172f2f7c1e9aeff079e0f1a1d57c7ea15c20bb7b
Terence Qu <therealterencequ@gmail.com>
2022-11-27 03:55:28 +1300

    Publishing script

--- publish.sh
+++ publish.sh
@@ -1,8 +1,14 @@
-npm version patch -m "Bump version to %s"
+# Version incrementing
+npm config set sign-git-tag false
+npm version patch --git-tag-version false # Increment patch number to get package version for everything
 PACKAGE_VERSION=$(cat package.json|grep version|head -1|awk -F: '{ print $2 }'|sed 's/[", ]//g')
-git tag -a "v$PACKAGE_VERSION" -m "Version $PACKAGE_VERSION"
-git push origin "v$PACKAGE_VERSION"
-git push
+
+# Katana - Core: Increment version and publish to npm
+cd ./projects/core/
+npm version $PACKAGE_VERSION --git-tag-version false
+cd ../../dist/core/
 npm publish --access public
-read -s -n 1 -p "Press any key to continue . . ."
-echo ""
+cd ../../
+
+# Finalise version change, and push to git
+npm version $PACKAGE_VERSION --allow-same-version true
