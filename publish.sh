8c75b4c05394bf1dfe583b46da8c4597e19c303c
Terence Qu <therealterencequ@gmail.com>
2022-11-27 04:42:37 +1300

    Bump version to 0.0.13

--- publish.sh
+++ publish.sh
@@ -1,12 +1,17 @@
-# Version incrementing
+# Init
+npm install
 npm config set sign-git-tag false
+
+# Version incrementing
 npm version patch --git-tag-version false # Increment patch number to get package version for everything
 PACKAGE_VERSION=$(cat package.json|grep version|head -1|awk -F: '{ print $2 }'|sed 's/[", ]//g')
 
 # Katana - Core: Increment version and publish to npm
 cd ./projects/core/
 npm version $PACKAGE_VERSION --git-tag-version false
-cd ../../dist/core/
+cd ../../
+ng build core
+cd ./dist/core/
 npm publish --access public
 cd ../../
 
